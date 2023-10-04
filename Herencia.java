52e96e63d3654c504ec87be2e376441cd9e90ef0
Valentina Segura Solis <98981414+val-02s@users.noreply.github.com>
2023-10-04 09:23:35 -0700

    Add files via upload

--- Herencia.java
+++ Herencia.java
@@ -4,66 +4,47 @@ public class Herencia{
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
 		
-		Alumno alus[] = new Alumno[3];
-		
-		alus[0] = new Alumno();
-		alus[0].setNombre("Valentina");
-		alus[0].setMatricula(1282007);
-		alus[0].setCorreo("valentina.segura@uabc.edu.mx");
-		alus[0].setTelefono("6648137183");
-		alus[0].setPromedio(79.9);
+		Alumno alus[] = new Alumno[CapturaEntrada.capturarEntero("Introduzca el numero de alumnos:")];
 
-		alus[1] = new Alumno();
-		System.out.println("Captura el nombre del alumno: ");
-		alus[1].setNombre(scan.nextLine());
-		System.out.println("Captura la matricula del alumno: ");
-		alus[1].setMatricula(Integer.parseInt(scan.nextLine()));
-		System.out.println("Captura el correo del alumno: ");
-		alus[1].setCorreo(scan.nextLine());
-		System.out.println("Captura el telefono del alumno: ");
-		alus[1].setTelefono(scan.nextLine());
-		System.out.println("Captura el promedio del alumno: ");
-		alus[1].setPromedio(Double.parseDouble(scan.nextLine()));
-
-		alus[2] = new Alumno();
-		if  (args.length > 4){
-			alus[2].setNombre(args[0]);
-			alus[2].setMatricula(Integer.parseInt(args[1]));
-			alus[2].setCorreo(args[2]);
-			alus[2].setTelefono(args[3]);
-			alus[2].setPromedio(Double.parseDouble(args[4]));
-		}
-		else System.out.println("uso: java MisAlumnos nombere matricula correo telefono promedio");
-
-        Profesor profe[] = new Profesor[2];
-
-        profe[0] = new Profesor();
-		profe[0].setNombre("Pepe Chuy");
-		profe[0].setNumEmp(123456);
-		profe[0].setCorreo("Pepe.Chuy@uabc.edu.mx");
-		profe[0].setTelefono("6634856789");
-		profe[0].setSueldo(5000);
-
-        profe[1] = new Profesor();
-		System.out.println("Captura el nombre del Profesor: ");
-		profe[1].setNombre(scan.nextLine());
-		System.out.println("Captura el numero de empleado del profesor: ");
-		profe[1].setNumEmp(Integer.parseInt(scan.nextLine()));
-		System.out.println("Captura el correo del profesor: ");
-		profe[1].setCorreo(scan.nextLine());
-		System.out.println("Captura el telefono del profesor: ");
-		profe[1].setTelefono(scan.nextLine());
-		System.out.println("Captura el sueldo del profesor: ");
-		profe[1].setSueldo(Double.parseDouble(scan.nextLine()));
-
-		System.out.println("Datos Alumnos:");
 		for(int i = 0; i<alus.length; i++){
-			if (alus[i].getNombre() != null) System.out.println(alus[i].getNombre()+"\t"+alus[i].getMatricula()+"\t"+alus[i].getCorreo()+"\t"+alus[i].getTelefono()+"\t"+alus[i].getPromedio());
+			System.out.printf("Datos del Alumno %s\n", i, ":");
+			alus[i] = new Alumno();
+			System.out.println("Captura el nombre del alumno: ");
+			alus[i].setNombre(scan.nextLine());
+			System.out.println("Captura la matricula del alumno: ");
+			alus[i].setMatricula(Integer.parseInt(scan.nextLine()));
+			System.out.println("Captura el correo del alumno: ");
+			alus[i].setCorreo(scan.nextLine());
+			System.out.println("Captura el telefono del alumno: ");
+			alus[i].setTelefono(scan.nextLine());
+			System.out.println("Captura el promedio del alumno: ");
+			alus[i].setPromedio(Double.parseDouble(scan.nextLine()));
 		}
 
-        System.out.println("Datos Profesores:");
-		for(int i = 0; i<profe.length; i++){
-			if (profe[i].getNombre() != null) System.out.println(profe[i].getNombre()+"\t"+profe[i].getNumEmp()+"\t"+profe[i].getCorreo()+"\t"+profe[i].getTelefono()+"\t"+profe[i].getSueldo());
+		Profesor profe[] = new Profesor[1];
+		for(int n = 0; n<profe.length; n++){
+			profe[0] = new Profesor();
+			System.out.println("Datos del Profesor:");
+			System.out.println("Captura el nombre del Profesor: ");
+			profe[0].setNombre(scan.nextLine());
+			System.out.println("Captura el numero de empleado del profesor: ");
+			profe[0].setNumEmp(Integer.parseInt(scan.nextLine()));
+			System.out.println("Captura el correo del profesor: ");
+			profe[0].setCorreo(scan.nextLine());
+			System.out.println("Captura el telefono del profesor: ");
+			profe[0].setTelefono(scan.nextLine());
+			System.out.println("Captura el sueldo del profesor: ");
+			profe[0].setSueldo(Double.parseDouble(scan.nextLine()));
+		}
+		
+		for(int j = 0; j<alus.length; j++){
+			System.out.printf("Datos Alumno %s:\n", j);
+			if (alus[j].getNombre() != null) System.out.println(alus[j].getNombre()+"\t"+alus[j].getMatricula()+"\t"+alus[j].getCorreo()+"\t"+alus[j].getTelefono()+"\t"+alus[j].getPromedio());
+		}
+
+        System.out.println("Datos Profesor:");
+		for(int k = 0; k<profe.length; k++){
+			if (profe[k].getNombre() != null) System.out.println(profe[k].getNombre()+"\t"+profe[k].getNumEmp()+"\t"+profe[k].getCorreo()+"\t"+profe[k].getTelefono()+"\t"+profe[k].getSueldo());
 		}
 	}
 }
@@ -143,4 +124,11 @@ class Profesor extends Persona{
     }
 }
 
+class CapturaEntrada{
 
+    public static int capturarEntero(String i){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(""+i+"");
+        return (sc.nextInt());
+    }
+}
